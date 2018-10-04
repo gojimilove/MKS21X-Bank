232d8b1c0924df8ace96bdc2d415fed6822d0647
Jessica Kim <jkim00@stuy.edu>
2018-10-04 09:55:38 -0400

    working bankAccount plus new driver from class

--- BankAccount.java
+++ BankAccount.java
@@ -1,7 +1,32 @@
 public class BankAccount {
-	public int balance;
+	public double balance;
 	public int accountID;
-	public BankAccount(int startBalance, int startID) {
-		
+	public String password;
+	public BankAccount(double startBalance, int startID, String startPass) {
+		balance = startBalance;
+		accountID = startID;
+		password = startPass;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public int getID() {
+		return accountID;
+	}
+	public void setPassword(String newPass) {
+		password = newPass;
+	}
+	public String toString() {
+		return accountID + "\t" + balance;
+	}
+	public boolean deposit(double deposit) {
+		if (deposit < 0) return false;		
+		balance+= deposit;
+		return true;
+	}
+	public boolean withdraw(double withdrawal) {
+		if (withdrawal > balance) return false;		
+		balance -= withdrawal;
+		return true;
 	}
 }
